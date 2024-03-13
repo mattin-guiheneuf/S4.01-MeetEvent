176a74fd4ef4c213019fd564a73c4f5a094d927b
Yannis Duvignau <duvignau.yannis@gmail.com>
2024-03-13 16:14:02 +0100

    ajout mail

--- src/siteweb/enregistrement.php
+++ src/siteweb/enregistrement.php
@@ -154,9 +154,10 @@ if (isset($_POST["email"]) || isset($_POST["pseudo"]) || isset($_POST["dateNaiss
             </html>
             ';
 
-            $headers = 'From: contact.meetevent@gmail.com' . "\r\n" .
-                'Content-type: text/html; charset=utf-8';
-
+            $headers =  "From: contact.meetevent@gmail.com" . "\r\n" .
+                        "Content-type: text/html; charset=utf-8" . "\r\n" .
+                        "Reply-To: " . $_POST["email"] . "\r\n" .
+                        "X-Mailer: PHP/" . phpversion();
             $mail_sent = mail($to, $subject, $message, $headers);
 
             if (!$mail_sent) {
