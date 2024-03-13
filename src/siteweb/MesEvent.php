fb7815d0ff26adf0644ebf4152ca52e3996e4727
Mattin Guiheneuf <mguiheneuf@iutbayonne.univ-pau.fr>
2024-03-13 10:53:14 +0100

    Corecction lien

--- src/siteweb/MesEvent.php
+++ src/siteweb/MesEvent.php
@@ -77,7 +77,7 @@ if (!isset($_SESSION['user_id'])) {
                 <button class="btn-consulter">
                     CONSULTEZ VOS EVENEMENTS
                 </button>
-                <button class="btn-creation" href="creaEvent.php">
+                <button class="btn-creation" onclick="window.location.href = 'creaEvent.php'">
                     CREEZ VOTRE EVENEMENT
                 </button>
             </div>
